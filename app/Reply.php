11ff60c78ae7b8e36cad888065b059bb6252d424
SublimePierreAlexis <pierre-alexis@sublimeskinz.com>
2018-01-22 17:13:46 +0100

    Episode 59 ok

--- app/Reply.php
+++ app/Reply.php
@@ -74,9 +74,14 @@ class Reply extends Model
         return $this->thread->path() . "#reply-{$this->id}";
     }
 
+    public function setBodyAttribute($body)
+    {
+        $this->attributes['body'] = preg_replace('/@([\w\-]+)/', '<a href="/profiles/$1">$0</a>', $body);
+    }
+
     public function mentionedusers()
     {
-        preg_match_all('/\@([^\s\.]+)/', $this->body, $matches);
+        preg_match_all('/@([\w\-]+)/', $this->body, $matches);
 
         return $matches[1];
     }
--- tests/Unit/ReplyTest.php
+++ tests/Unit/ReplyTest.php
@@ -34,10 +34,23 @@ class ReplyTest extends TestCase
     /** @test */
     public function it_can_detect_all_mentioned_users_in_the_body()
     {
-        $reply = create('App\Reply', [
+        $reply = new \App\Reply([
             'body' => '@JaneDoe wants to talk to @JohnDoe',
         ]);
 
         $this->assertEquals(['JaneDoe', 'JohnDoe'], $reply->mentionedusers());
+    }
+
+    /** @test */
+    public function it_wraps_usernames_in_the_body_within_anchor_tags()
+    {
+        $reply = new \App\Reply([
+            'body' => 'Hello @Jane-Doe.',
+        ]);
+
+        $this->assertEquals(
+            'Hello <a href="/profiles/Jane-Doe">@Jane-Doe</a>.',
+            $reply->body
+        );
     }
 }
