cc5b7e2f74c826694327ebc495bb48da2baf1ce8
SublimePierreAlexis <pierre-alexis@sublimeskinz.com>
2018-01-29 15:51:47 +0100

    Episode 66 ok

--- app/Http/Controllers/ThreadsController.php
+++ app/Http/Controllers/ThreadsController.php
@@ -6,6 +6,7 @@ use App\Channel;
 use App\Thread;
 use App\Filters\ThreadsFilters;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Redis;
 
 class ThreadsController extends Controller
 {
@@ -36,7 +37,9 @@ class ThreadsController extends Controller
             return $threads;
         }
 
-        return view('threads.index', compact('threads'));
+        $trending = array_map('json_decode', Redis::zrevrange('trending_threads', 0, 4));
+
+        return view('threads.index', compact('threads', 'trending'));
     }
 
     /**
@@ -88,6 +91,11 @@ class ThreadsController extends Controller
         if (auth()->check()) {
             auth()->user()->read($thread);
         }
+
+        Redis::zincrby('trending_threads', 1, json_encode([
+            'title' => $thread->title,
+            'path'  => $thread->path(),
+        ]));
 
         return view('threads.show', compact('thread'));
     }
--- resources/views/threads/index.blade.php
+++ resources/views/threads/index.blade.php
@@ -3,10 +3,26 @@
 @section('content')
     <div class="container">
         <div class="row">
-            <div class="col-md-8 col-md-offset-2">
+            <div class="col-md-8">
                 @include('threads._list')
 
                 {{ $threads->render() }}
+            </div>
+
+            <div class="col-md-4">
+                <div class="panel panel-default">
+                    <div class="panel-heading">
+                        Trending threads
+                    </div>
+
+                    <div class="panel-body">
+                        @foreach($trending as $thread)
+                            <li class="list-group-item">
+                                <a href="{{ url($thread->path) }}">{{ $thread->title }}</a>
+                            </li>
+                        @endforeach
+                    </div>
+                </div>
             </div>
         </div>
     </div>
